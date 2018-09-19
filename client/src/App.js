0baf02cec4deaf277f2f4e2a914b80baded99716
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-19 14:51:53 -0600

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import NoMatch from './components/NoMatch'
+import FetchApps from './components/FetchApps'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/apps" component={FetchApps} />
+    <Route component={NoMatch} />
+  </Switch>
+)
 
-export default App;
+export default App
---
+++ client/src/components/Apps.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import {
+  Container, 
+  Header,
+  Card,
+  Image,
+  Dropdown,
+  Divider,
+  Button,
+} from 'semantic-ui-react'
+
+class Apps extends React.Component {
+  state = { category: '' }
+
+  categoryOptions = () => {
+    const { categories } = this.props
+    return categories.map( (c) => { return { key: c, text: c, value: c } } )
+  }
+
+  apps = () => {
+    const { apps } = this.props
+    const { category } = this.state
+
+    let visible = apps
+
+    if (category) 
+      visible = apps.filter( a => a.category === category )
+
+    return visible.map( app => {
+      const { name, id, category, author, logo } = app
+      return (
+        <Card key={id}>
+          <Image src={logo} />
+          <Card.Content>
+            <Card.Header>
+              {name}
+            </Card.Header>
+            <Card.Meta>
+              <span>{author}</span>
+            </Card.Meta>
+            <Card.Description>
+              {category}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/apps/${app.id}`}>
+              View App
+            </Link>
+          </Card.Content>
+        </Card>
+      )
+    })
+  }
+
+  handleChange = (_, { value }) => {
+    this.setState({ category: value })
+  }
+
+  render() {
+    const { category } = this.state
+
+    return (
+      <Container>
+        <Header as="h3" textAlign="center">Apps</Header>
+        <Dropdown
+          placeholder="Filter by..."
+          fluid
+          selection
+          options={this.categoryOptions()}
+          value={category}
+          onChange={this.handleChange}
+        />
+        { category && 
+            <Button
+              fluid
+              basic
+              onClick={ () => this.setState({ category: '' }) }
+            >
+              Clear Filter: {category}
+            </Button>
+        }
+        <Divider />
+        <Card.Group itemsPerRow={4} stackable>
+          { this.apps() }
+        </Card.Group>
+      </Container>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { apps } = state
+  const categories = [...new Set(apps.map( a => a.category ))]
+  return { apps, categories }
+}
+
+export default connect(mapStateToProps)(Apps)
