7145ba6a17f29e0fffdbf98f14ce62fa78dc7550
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-09 13:05:35 -0700

    index setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
@@ -17,4 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
