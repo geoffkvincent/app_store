26621f801c20adaafe60450db8f4da954fea44ef
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-09 13:38:49 -0700

    action functions

--- client/src/reducers/apps.js
+++ client/src/reducers/apps.js
@@ -4,3 +4,19 @@ const APPS = 'APPS'
 const ADD_APP = 'ADD_APP'
 const UPDATE_APP = 'UPDATE_APP'
 const DELETE_APP = 'DELETE_APP'
+
+export const getApps = () => {
+
+}
+
+export const addApp = (app) => {
+
+}
+
+export const updateApp = (app) => {
+
+}
+
+export const deleteApp = (id) => {
+
+}
