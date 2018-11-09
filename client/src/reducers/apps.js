5343530feb591438e98e47801741bf34a5b2faf9
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-09 14:16:43 -0700

    reducer function

--- client/src/reducers/apps.js
+++ client/src/reducers/apps.js
@@ -6,7 +6,7 @@ const UPDATE_APP = 'UPDATE_APP'
 const DELETE_APP = 'DELETE_APP'
 
 export const getApps = () => {
-
+  axios.get('api/app')
 }
 
 export const addApp = (app) => {
@@ -19,4 +19,23 @@ export const updateApp = (app) => {
 
 export const deleteApp = (id) => {
 
+}
+
+export default (state = [], action) {
+  switch(action.type) {
+    case APPS:
+      return action.apps
+    case ADD_APP:
+      return [action.app, ...state]
+    case UPDATE_APP:
+      return state.map(a => {
+        if ( a.id === action.app.id)
+          return action.app
+        return a
+      })
+    case DELETE_APP:
+      return state.filter( a => a.id !== action.id )
+    default:
+      return state
+  }
 }
