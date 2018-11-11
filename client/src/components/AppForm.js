c370e675c18ffc46f00f028c7568849a9ec5e244
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-11 10:36:58 -0700

    form setup

---
+++ client/src/components/AppForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {updateApp, addApp} from '../reducers/apps'
+import {Form} from 'semantic-ui-react'
+
+class AppForm extends React.Component {
+
+}
+
+export default connect()(AppForm)
